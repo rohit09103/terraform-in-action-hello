5632e5638436d8b67c90a1a385592346a9f2a8c1
Rohit <rohitsharma173120@gmail.com>
2022-06-06 14:31:23 +0530

    Added httpd

--- main.tf
+++ main.tf
@@ -16,4 +16,11 @@ provider "aws" {
 resource "aws_instance" "helloworld" {
   ami = "ami-041d6256ed0f2061c"
   instance_type = "t2.micro"
+  user_data = <<-EOF
+              #!/bin/bash
+              sudo yum update -y
+              sudo yum install httpd -y
+              sudo systemctl start httpd
+              sudo bash -c "echo My First web server from terraform > /var/www/html/index.html"
+              EOF
 }
